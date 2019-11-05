b81ef09d4f85b67b8fdef19a2f721ffa876391b5
dzucconi <mail@damonzucconi.com>
2019-11-05 13:29:13 -0500

    Bootstraps a documentation site

---
+++ documentation/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "@moda/om";
+
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>@moda/om</h1>
+
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default App;
