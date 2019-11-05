bce9fba4b9118ee440ba9476e591023e5e3610bd
dzucconi <mail@damonzucconi.com>
2019-11-05 17:37:22 -0500

    test(jest): sets up jest
    Closes #4

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import { Button } from "./Button";
+
+describe("Button", () => {
+  it("renders correctly", () => {
+    const button = shallow(<Button>Click me</Button>);
+    expect(button.text()).toEqual("Click me");
+  });
+});
