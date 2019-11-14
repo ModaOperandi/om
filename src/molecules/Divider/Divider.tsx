345dce4bb8e4a2300438357b2fd40086049a355e
Sam Jacobs <sam.jacobs@modaoperandi.com>
2019-11-13 18:28:52 -0600

    refactor(divider): reformat indents, remove hardcode px [DOOR-25]

--- src/molecules/Divider/Divider.tsx
+++ src/molecules/Divider/Divider.tsx
@@ -3,23 +3,23 @@ import React from 'react';
 import './Divider.scss';
 
 export interface Props {
-    text: string;
-    noLine?: boolean;
-    doubleLine?: boolean;
+  text: string;
+  noLine?: boolean;
+  doubleLine?: boolean;
 }
 
-export const Divider : React.FC<Props> = ({text, noLine, doubleLine}) => {
-    const classes = classNames({
-        'Divider': true,
-        'Divider--no-line': noLine,
-        'Divider--is-double': doubleLine
-    });
+export const Divider: React.FC<Props> = ({text, noLine, doubleLine}) => {
+  const classes = classNames({
+    'Divider': true,
+    'Divider--no-line': noLine,
+    'Divider--is-double': doubleLine
+  });
 
-    return (
-        <div className={classes}>
-            <hr className="Divider__left"/>
-            <div className="Divider__text">{text}</div>
-            <hr className="Divider__right"/>
-        </div>
-    );
+  return (
+    <div className={classes}>
+      <hr className="Divider__left"/>
+      <div className="Divider__text">{text}</div>
+      <hr className="Divider__right"/>
+    </div>
+  );
 };
