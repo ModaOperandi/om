0bdd447f90167ffe14529ba4e1b820326594d751
dzucconi <mail@damonzucconi.com>
2019-11-15 12:59:54 -0500

    feat(molecules): exposes SiteFooter

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { Button } from './components/Button';
 export { NavBar } from './molecules/NavBar';
 export { TopNav } from './molecules/TopNav';
 export { Divider, TwoLineDivider, NoLineDivider } from './molecules/Divider';
+export { SiteFooter } from './molecules/SiteFooter';
