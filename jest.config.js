5c0221a7fb456415306646bc620c603988661949
dzucconi <mail@damonzucconi.com>
2019-11-15 12:51:44 -0500

    feat(molecules): roughs in a SiteFooter component

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  moduleNameMapper: { '\\.(scss)$': 'identity-obj-proxy' },
+  moduleNameMapper: {
+    '\\.(scss)$': 'identity-obj-proxy',
+    '@moda/icons': '<rootDir>/src/__mocks__/Icon.tsx'
+  },
   preset: 'ts-jest',
   setupFiles: ['<rootDir>/enzyme.config.js'],
   testEnvironment: 'node'
