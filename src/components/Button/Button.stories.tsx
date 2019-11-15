c91659e523dc44b519c69f7571f34c048d5f6f77
dzucconi <mail@damonzucconi.com>
2019-11-15 15:51:13 -0500

    feat(buttons): roughs in primary/secondary buttons + states

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -5,4 +5,52 @@ import { Button } from './Button';
 
 export default { title: 'Components|Button' };
 
-export const ButtonWithLabel = () => <Button onClick={action('clicked')}>Click Me</Button>;
+export const Primary = () => (
+  <>
+    <p>
+      <Button onClick={action('clicked')}>Add to cart</Button>
+    </p>
+    <p>
+      <Button hover onClick={action('clicked')}>
+        Add to cart
+      </Button>
+    </p>
+
+    <p>
+      <Button focus onClick={action('clicked')}>
+        Add to cart
+      </Button>
+    </p>
+
+    <p>
+      <Button disabled>Please select a size first</Button>
+    </p>
+  </>
+);
+
+export const Secondary = () => (
+  <>
+    <p>
+      <Button secondary onClick={action('clicked')}>
+        Add to cart
+      </Button>
+    </p>
+    <p>
+      <Button secondary hover onClick={action('clicked')}>
+        Add to cart
+      </Button>
+    </p>
+
+    <p>
+      <Button secondary focus onClick={action('clicked')}>
+        Add to cart
+      </Button>
+    </p>
+
+    <p>
+      <Button secondary disabled>
+        Please select a size first
+      </Button>
+    </p>
+  </>
+);
