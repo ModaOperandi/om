639fa525f337e16f634cec115435d3b83b076e8f
dzucconi <mail@damonzucconi.com>
2019-11-18 12:33:45 -0500

    fix(navbar): fixes flash of open nav when rendered server-side

--- src/molecules/NavBar/NavBar.tsx
+++ src/molecules/NavBar/NavBar.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 import { NavBarPulldown } from './NavBarPulldown';
 import { Subsection, Link } from './NavBarPulldownSubsection';
@@ -14,8 +15,19 @@ export interface Props {
 }
 
 export const NavBar: React.FC<Props> = ({ sections, ...rest }) => {
+  const [isInitialized, initialize] = useState(false);
+
+  useEffect(() => {
+    initialize(true);
+  }, []);
+
   return (
-    <nav className='NavBar' {...rest}>
+    <nav
+      className={classNames('NavBar', {
+        'NavBar--initialized': isInitialized
+      })}
+      {...rest}
+    >
       {sections.map(section => (
         <div className='NavBar__section' key={section.href}>
           <a href={section.href}>{section.label}</a>
