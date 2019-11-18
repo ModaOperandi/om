7f060a4d0e74b6dd7db1e665fd0c36b47a22301e
Sam Jacobs <sam.jacobs@modaoperandi.com>
2019-11-18 11:54:10 -0600

    refactor(divider): ...rest for style [DOOR-25]

--- src/molecules/Divider/Divider.tsx
+++ src/molecules/Divider/Divider.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import './Divider.scss';
 
-export interface Props {
+export interface Props extends React.HTMLAttributes<HTMLDivElement> {
   text: string;
   type?: 'no-line' | 'two-line';
   verticalPadding?: string;
 }
 
-export const Divider: React.FC<Props> = ({ text, type, verticalPadding }) => {
-  let padding = '';
-  if (verticalPadding) {
-    padding = `${verticalPadding} 0`;
-  }
-
+export const Divider: React.FC<Props> = ({ text, type, ...rest }) => {
   return (
-    <div className={'Divider ' + (type ? `Divider--${type}` : '')} style={{ padding }}>
+    <div className={'Divider ' + (type ? `Divider--${type}` : '')} style={rest}>
       <div className='Divider__text'>{text}</div>
     </div>
   );
