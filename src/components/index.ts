e22d412eedd972f3bae847e8d94189300cb78ea4
dzucconi <mail@damonzucconi.com>
2019-11-19 10:49:10 -0500

    refactor(index): exports indexes for each: components/molecules

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { Button } from './Button';
+export { Breakpoint } from './Breakpoint';
+export { Breadcrumbs } from './Breadcrumbs';
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-// Components
-export { Button } from './components/Button';
-export { Breakpoint } from './components/Breakpoint';
-
-// Molecules
-export { NavBar } from './molecules/NavBar';
-export { TopNav } from './molecules/TopNav';
-export { Divider, TwoLineDivider, NoLineDivider, DividerProps } from './molecules/Divider';
-export { SiteFooter } from './molecules/SiteFooter';
+export * from './components';
+export * from './molecules';
