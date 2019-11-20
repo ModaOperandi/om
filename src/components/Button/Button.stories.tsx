cbc89e60844d4034a3e88d47c70ddfe6276e802b
dzucconi <mail@damonzucconi.com>
2019-11-20 10:42:29 -0500

    fix(button): supports icons

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,6 +3,7 @@ import { action } from '@storybook/addon-actions';
 
 import { States } from '../../utilities/States';
 import { Button } from './Button';
+import Heart from '@moda/icons/favorite-outline-16';
 
 export default { title: 'Components|Button' };
 
@@ -24,6 +25,22 @@ export const PrimaryWithHref = () => (
   <States states={[null, { hover: true }, { focus: true }, { disabled: true }]}>
     <Button href='#href' onClick={action('clicked')}>
       Add to Cart
+    </Button>
+  </States>
+);
+
+export const PrimaryWithIcon = () => (
+  <States states={[null, { hover: true }, { focus: true }, { disabled: true }]}>
+    <Button onClick={action('clicked')}>
+      <Heart /> Add to Favorites
+    </Button>
+  </States>
+);
+
+export const SecondaryWithIcon = () => (
+  <States states={[null, { hover: true }, { focus: true }, { disabled: true }]}>
+    <Button secondary onClick={action('clicked')}>
+      <Heart /> Add to Favorites
     </Button>
   </States>
 );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,33 +10,33 @@ type CommonProps = {
   disabled?: boolean;
 };
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  href?: undefined;
-};
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+type AnchorProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
-type AnchorProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
-  href?: string;
-};
-
-const hasHref = (props: ButtonProps | AnchorProps): props is AnchorProps => 'href' in props;
+const isAnchor = (props: ButtonProps | AnchorProps): props is AnchorProps => 'href' in props;
+const isButton = (props: ButtonProps | AnchorProps): props is ButtonProps => !('href' in props);
 
 export const Button: React.FC<CommonProps & (ButtonProps | AnchorProps)> = ({
   secondary,
   hover,
   focus,
   disabled,
+  className,
   ...rest
 }) => {
-  const className = classNames('Button', {
-    'Button--secondary': secondary,
-    'Button--hover': hover,
-    'Button--focus': focus,
-    'Button--disabled': disabled
-  });
-
-  return hasHref(rest) ? (
-    <a className={className} {...rest} />
-  ) : (
-    <button disabled={disabled} className={className} {...rest} />
+  const cn = classNames(
+    'Button',
+    {
+      'Button--secondary': secondary,
+      'Button--hover': hover,
+      'Button--focus': focus,
+      'Button--disabled': disabled
+    },
+    className
   );
+
+  if (isAnchor(rest)) return <a className={cn} {...rest} />;
+  if (isButton(rest)) return <button disabled={disabled} className={cn} {...rest} />;
+
+  return null;
 };
