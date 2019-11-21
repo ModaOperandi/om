f2a3977166c0edfa233888505c32ba787114ae3e
dzucconi <mail@damonzucconi.com>
2019-11-21 14:49:16 -0500

    chore(stories): adds another dl example

--- src/components/DefinitionList/DefinitionList.stories.tsx
+++ src/components/DefinitionList/DefinitionList.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { DefinitionList } from './DefinitionList';
+import { Text } from '../Text';
 
 export default { title: 'Components|DefinitionList' };
 
@@ -8,7 +9,9 @@ export const Default = () => (
   <>
     <DefinitionList term='Color'>Black</DefinitionList>
     <DefinitionList term='Size'>Please select a size</DefinitionList>
-    <DefinitionList term='Size'>US 2</DefinitionList>
+    <DefinitionList term='Size'>
+      US 2 — <Text color='code-red'>Only 2 Left</Text>
+    </DefinitionList>
     <DefinitionList term='Supports'>
       <a href='#link'>Links</a>
     </DefinitionList>
