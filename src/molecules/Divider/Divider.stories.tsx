2be1ab76ce929a0c839d351755179c690d79dd75
dzucconi <mail@damonzucconi.com>
2019-11-22 12:03:12 -0500

    chore(divider): cleans up divider story; adds test

--- src/molecules/Divider/Divider.stories.tsx
+++ src/molecules/Divider/Divider.stories.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { States } from '../../utilities/States';
 import { Divider, NoLineDivider, TwoLineDivider } from './';
 
 export default { title: 'Molecules|Divider' };
 
-export const Default = () => <Divider style={{ padding: '30px 0' }} text='Default Divider' />;
+export const Default = () => (
+  <States states={[{}, { type: 'no-line' }, { type: 'two-line' }]}>
+    <Divider text='Moda Operandi' />
+  </States>
+);
 
 export const withNoLine = () => <NoLineDivider text='Divider with No Line' />;
 
