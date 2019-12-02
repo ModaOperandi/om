1d48d33cdca512efdbdb38f1a04385fd39a677a3
dzucconi <mail@damonzucconi.com>
2019-12-02 17:04:19 -0500

    chore(states): cleans up props display

---
+++ src/utilities/States/stringifyProps.tsx
@@ -0,0 +1,12 @@
+export const stringifyProps = (props: any) => {
+  const stringifiedProps = Object.entries(props || {}).reduce((memo: string, [key, value]) => {
+    if (key === 'children') return `${memo} children={...}`;
+    if (typeof value === 'string') return `${memo} ${key}="${value}"`;
+    if (typeof value === 'function') return `${memo} ${key}={fn}`;
+    if (typeof value === 'boolean') return `${memo} ${key}${value ? '' : '={false}'}`;
+    if (typeof value === 'number') return `${key}={${value}}`;
+    return `${memo} ${key}=${value}`;
+  }, '');
+
+  return stringifiedProps.trim();
+};
