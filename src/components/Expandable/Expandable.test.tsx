38f4e4d78e009523a5e517c9ca65d09c360cceb0
dzucconi <mail@damonzucconi.com>
2019-12-02 18:10:35 -0500

    refactor(clickables): utilize Clickable in place of button

--- src/components/Expandable/Expandable.test.tsx
+++ src/components/Expandable/Expandable.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import { Expandable } from './Expandable';
 
 describe('Expandable', () => {
   it('expands when clicked', () => {
-    const component = shallow(<Expandable name='Hello'>Goodbye</Expandable>);
+    const component = mount(<Expandable name='Hello'>Goodbye</Expandable>);
     expect(component.html()).not.toContain('expanded');
     component.find('button').simulate('click');
     expect(component.html()).toContain('expanded');
