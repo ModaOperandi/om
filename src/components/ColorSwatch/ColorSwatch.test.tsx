b8d16382a376e55abf4a76f9eb26b5ccb8ea9497
dzucconi <mail@damonzucconi.com>
2019-12-02 17:44:17 -0500

    feat(colorswatch): supports onClick

--- src/components/ColorSwatch/ColorSwatch.test.tsx
+++ src/components/ColorSwatch/ColorSwatch.test.tsx
@@ -7,7 +7,7 @@ describe('ColorSwatch', () => {
   it('renders correctly', () => {
     const component = shallow(<ColorSwatch color='nude' />);
     expect(component.html()).toEqual(
-      '<div class="ColorSwatch"><div class="ColorSwatch__chip" style="background-color:#eee4d7"></div></div>'
+      '<button class="ColorSwatch"><div class="ColorSwatch__chip" style="background-color:#eee4d7"></div></button>'
     );
   });
 });
