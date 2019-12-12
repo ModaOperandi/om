dad43a3b56d233967f89106b19971b6417575622
dzucconi <mail@damonzucconi.com>
2019-12-12 12:49:29 -0500

    <back>

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import { action } from '@storybook/addon-actions';
 
 import { States } from '../../utilities/States';
@@ -8,14 +8,23 @@ import Heart from '@moda/icons/favorite-outline-16';
 
 export default { title: 'Components|Button' };
 
+const STATES = [
+  null,
+  { hover: true },
+  { focus: true },
+  { disabled: true },
+  { loading: true },
+  { success: true }
+];
+
 export const Primary = () => (
-  <States states={[null, { hover: true }, { focus: true }, { disabled: true }]}>
+  <States states={STATES}>
     <Button onClick={action('clicked')}>Add to Cart</Button>
   </States>
 );
 
 export const Secondary = () => (
-  <States states={[null, { hover: true }, { focus: true }, { disabled: true }]}>
+  <States states={STATES}>
     <Button secondary onClick={action('clicked')}>
       Add to Cart
     </Button>
@@ -23,7 +32,7 @@ export const Secondary = () => (
 );
 
 export const PrimaryWithHref = () => (
-  <States states={[null, { hover: true }, { focus: true }, { disabled: true }]}>
+  <States states={STATES}>
     <Button href='#href' onClick={action('clicked')}>
       Add to Cart
     </Button>
@@ -31,7 +40,7 @@ export const PrimaryWithHref = () => (
 );
 
 export const PrimaryWithIcon = () => (
-  <States states={[null, { hover: true }, { focus: true }, { disabled: true }]}>
+  <States states={STATES}>
     <Button onClick={action('clicked')}>
       <Heart /> Add to Favorites
     </Button>
@@ -39,7 +48,7 @@ export const PrimaryWithIcon = () => (
 );
 
 export const SecondaryWithIcon = () => (
-  <States states={[null, { hover: true }, { focus: true }, { disabled: true }]}>
+  <States states={STATES}>
     <Button secondary onClick={action('clicked')}>
       <Heart /> Add to Favorites
     </Button>
@@ -54,3 +63,47 @@ export const VerticallyStacked = () => (
     </Button>
   </Stack>
 );
+
+VerticallyStacked.story = { parameters: { viewport: { defaultViewport: 'xs' } } };
+
+enum Mode {
+  Resting,
+  Loading,
+  Success
+}
+
+export const SimulateLoading = () => {
+  const [mode, setMode] = useState(Mode.Resting);
+  const timeout = useRef<NodeJS.Timeout | null>(null);
+  const handleClick = useCallback(() => {
+    setMode(Mode.Loading);
+  }, []);
+
+  useEffect(() => {
+    if (mode === Mode.Loading) {
+      timeout.current && clearTimeout(timeout.current);
+      timeout.current = setTimeout(() => setMode(Mode.Success), 2500);
+    }
+
+    if (mode === Mode.Success) {
+      timeout.current && clearTimeout(timeout.current);
+      timeout.current = setTimeout(() => setMode(Mode.Resting), 1000);
+    }
+  }, [mode]);
+
+  return (
+    <Stack space={2}>
+      <Button onClick={handleClick} loading={mode === Mode.Loading} success={mode === Mode.Success}>
+        {
+          {
+            [Mode.Resting]: 'Add to bag',
+            [Mode.Loading]: 'Add to bag',
+            [Mode.Success]: 'Item added to bag'
+          }[mode]
+        }
+      </Button>
+    </Stack>
+  );
+};
+
+SimulateLoading.story = { parameters: { viewport: { defaultViewport: 'xs' } } };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,6 +8,8 @@ type CommonProps = {
   focus?: boolean;
   hover?: boolean;
   disabled?: boolean;
+  loading?: boolean;
+  success?: boolean;
 };
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
@@ -21,7 +23,10 @@ export const Button: React.FC<CommonProps & (ButtonProps | AnchorProps)> = ({
   hover,
   focus,
   disabled,
+  loading,
+  success,
   className,
+  children,
   ...rest
 }) => {
   const cn = classNames(
@@ -30,13 +35,35 @@ export const Button: React.FC<CommonProps & (ButtonProps | AnchorProps)> = ({
       'Button--secondary': secondary,
       'Button--hover': hover,
       'Button--focus': focus,
-      'Button--disabled': disabled
+      'Button--disabled': disabled,
+      'Button--loading': loading,
+      'Button--success': success
     },
     className
   );
 
-  if (isAnchor(rest)) return <a className={cn} {...rest} />;
-  if (isButton(rest)) return <button disabled={disabled} className={cn} {...rest} />;
+  const __children__ = (
+    <>
+      {loading && <div className='Button__loader' />}
+      <span className='Button__children'>{children}</span>
+    </>
+  );
+
+  if (isAnchor(rest)) {
+    return (
+      <a className={cn} {...rest}>
+        {__children__}
+      </a>
+    );
+  }
+
+  if (isButton(rest)) {
+    return (
+      <button disabled={disabled} className={cn} {...rest}>
+        {__children__}
+      </button>
+    );
+  }
 
   return null;
 };
