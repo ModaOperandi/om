44a432278c05fed3f7ebde77f643e6f69198683e
dzucconi <mail@damonzucconi.com>
2020-01-07 13:16:32 -0500

    style: runs prettier over project

--- src/mixins/overflow-scroll/overflow-scroll.stories.tsx
+++ src/mixins/overflow-scroll/overflow-scroll.stories.tsx
@@ -1,12 +1,7 @@
-
 import React from 'react';
 
 import './overflow-scroll.stories.scss';
 
 export default { title: 'Mixins|overflow-scroll' };
 
-export const Default = () => (
-  <div className='Story Mixins overflow-scroll'>
-    Hello
-  </div>
-);
+export const Default = () => <div className='Story Mixins overflow-scroll'>Hello</div>;
--- src/mixins/visually-hidden/visually-hidden.stories.tsx
+++ src/mixins/visually-hidden/visually-hidden.stories.tsx
@@ -1,12 +1,7 @@
-
 import React from 'react';
 
 import './visually-hidden.stories.scss';
 
 export default { title: 'Mixins|visually-hidden' };
 
-export const Default = () => (
-  <div className='Story Mixins visually-hidden'>
-    Hello
-  </div>
-);
+export const Default = () => <div className='Story Mixins visually-hidden'>Hello</div>;
