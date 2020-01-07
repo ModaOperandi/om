a8970281e236fef5bb713c58f5b3d5a271516821
dzucconi <mail@damonzucconi.com>
2020-01-07 11:47:15 -0500

    feat(mixins): implements overflow-scroll

---
+++ src/mixins/overflow-scroll/overflow-scroll.stories.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+import './overflow-scroll.stories.scss';
+
+export default { title: 'Mixins|overflow-scroll' };
+
+export const Default = () => (
+  <div className='Story Mixins overflow-scroll'>
+    Hello
+  </div>
+);
