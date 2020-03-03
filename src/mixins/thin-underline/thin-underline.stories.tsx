718003f59fc55a6547ac521cfc95371421dc5e2f
dzucconi <mail@damonzucconi.com>
2020-03-03 15:20:47 -0500

    feat(mixins): ports over `thin-underline`

---
+++ src/mixins/thin-underline/thin-underline.stories.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+import './thin-underline.stories.scss';
+
+export default { title: 'Mixins|thin-underline' };
+
+export const Default = () => (
+  <div className='Story Mixins thin-underline'>
+    Hello
+  </div>
+);
