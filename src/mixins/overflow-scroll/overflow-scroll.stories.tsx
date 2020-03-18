c6ec27fec691882b1ea4e4589c1e26848a182049
Noah Schechter <n.schechter@s-macnyc002250.local>
2020-03-18 12:02:19 -0400

    feat: parameterize overflow-scroll

--- src/mixins/overflow-scroll/overflow-scroll.stories.tsx
+++ src/mixins/overflow-scroll/overflow-scroll.stories.tsx
@@ -4,4 +4,10 @@ import './overflow-scroll.stories.scss';
 
 export default { title: 'Mixins|overflow-scroll' };
 
-export const Default = () => <div className='Story Mixins overflow-scroll'>Hello</div>;
+export const Default = () => (
+  <div className='Story Mixins overflow-scroll'>Overflowed on the y axis!</div>
+);
+
+export const OverflowX = () => (
+  <div className='Story Mixins overflow-scroll-x'>Overflowed on the x axis!</div>
+);
