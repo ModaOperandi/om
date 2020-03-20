e06c21a1816065f1ef2428159035fd9719027176
dzucconi <mail@damonzucconi.com>
2020-03-20 15:57:04 -0400

    fix(radiobutton): updates radio button designs

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import classNames from 'classnames';
-import RadioSelectedIcon from '@moda/icons/radio-selected-16';
-import RadioUnselectedIcon from '@moda/icons/radio-unselected-16';
 import './RadioButton.scss';
 
 export type RadioButtonProps = React.InputHTMLAttributes<HTMLInputElement> & {
@@ -21,9 +19,11 @@ export const RadioButton: React.FC<RadioButtonProps> = ({
         'RadioButton--checked': checked
       })}
     >
-      <span className='RadioButton__indicator'>
-        {checked ? <RadioSelectedIcon /> : <RadioUnselectedIcon />}
-      </span>
+      <span
+        className={`RadioButton__indicator RadioButton__indicator--${
+          checked ? 'checked' : 'unchecked'
+        }`}
+      />
 
       <input
         className='RadioButton__input'
