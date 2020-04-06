5cbe5c335559221f9d3915a79eb46e1f82b4d388
dzucconi <mail@damonzucconi.com>
2020-04-06 15:27:51 -0400

    fix(modal): swaps out modal focusing lib

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
-import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
+import { FocusOn } from 'react-focus-on';
 import classNames from 'classnames';
 import './Modal.scss';
 
@@ -13,49 +13,25 @@ export const Modal: React.FC<ModalProps> = ({ className, children, onClose, over
   const wrapper = useRef(null);
   const el = useRef(document.createElement('div'));
 
-  const handleKeydown = React.useCallback(
-    (e: KeyboardEvent) => {
-      switch (e.key) {
-        case 'Escape':
-          return onClose();
-        default:
-          break;
-      }
-    },
-    [onClose]
-  );
-
-  const handleWrapperClick = React.useCallback(
-    (e: React.MouseEvent) => {
-      if (e.target === wrapper.current) {
-        onClose();
-      }
-    },
-    [onClose, wrapper]
-  );
-
   useEffect(() => {
     const _el = el.current;
 
     document.body.appendChild(_el);
-    disableBodyScroll(_el);
-    document.addEventListener('keydown', handleKeydown);
 
     return () => {
       _el && _el.parentElement && _el.parentElement.removeChild(_el);
-      clearAllBodyScrollLocks();
-      document.removeEventListener('keydown', handleKeydown);
     };
-  }, [handleKeydown]);
+  }, []);
 
   return createPortal(
     <div
       className={classNames('Modal', className, { 'Modal--overlay': overlay })}
-      onClick={handleWrapperClick}
       ref={wrapper}
       {...rest}
     >
-      {children}
+      <FocusOn onClickOutside={onClose} onEscapeKey={onClose}>
+        {children}
+      </FocusOn>
     </div>,
     el.current
   );
