01db3f67b4cee178f285d273b83febc4d80166c4
dzucconi <mail@damonzucconi.com>
2020-04-10 11:43:22 -0400

    refactor: spacing is now part of tokens

--- src/functions/spacing/spacing.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './spacing.stories.scss';
-
-export default { title: 'Functions|spacing' };
-
-export const Default = () => (
-  <div className='Story Functions spacing'>Inspect my margin/padding</div>
-);
