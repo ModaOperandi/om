e64e477285ed44663941e8612690e4f1b95f4dfc
Robert-Mihai Man <robertmihaiman7@gmail.com>
2020-05-04 14:28:36 +0300

    chore(<stack />): fix pr requets

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -3,14 +3,11 @@ import classNames from 'classnames';
 import './Stack.scss';
 import * as CSS from 'csstype';
 
-const defaultProperty = 'flex-start';
-
 export type StackProps = React.HTMLAttributes<HTMLDivElement> & {
   space: number;
   direction?: 'vertical' | 'horizontal';
   alignItems?: CSS.AlignItemsProperty;
   justifyContent?: CSS.JustifyContentProperty;
-  style?: undefined;
 };
 
 export const Stack: React.FC<StackProps> = ({
@@ -18,13 +15,14 @@ export const Stack: React.FC<StackProps> = ({
   children,
   space,
   direction = 'vertical',
-  alignItems = defaultProperty,
-  justifyContent = defaultProperty,
+  alignItems,
+  justifyContent,
+  style,
   ...rest
 }) => (
   <div
     className={classNames(`Stack Stack--${direction}-${space}`, className)}
-    style={{ alignItems, justifyContent }}
+    style={{ ...style, alignItems, justifyContent }}
     {...rest}
   >
     {children}
