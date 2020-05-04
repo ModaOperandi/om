5b07362e57151f2dafe24e8e96e7227c4c642c4a
Robert-Mihai Man <robertmihaiman7@gmail.com>
2020-05-04 13:39:02 +0300

    feat(<stack />): added justifyContent and alignItems props

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import classNames from 'classnames';
 import './Stack.scss';
+import * as CSS from 'csstype';
+
+const defaultProperty = 'flex-start';
 
 export type StackProps = React.HTMLAttributes<HTMLDivElement> & {
   space: number;
   direction?: 'vertical' | 'horizontal';
+  alignItems?: CSS.AlignItemsProperty;
+  justifyContent?: CSS.JustifyContentProperty;
+  style?: undefined;
 };
 
 export const Stack: React.FC<StackProps> = ({
@@ -12,9 +18,15 @@ export const Stack: React.FC<StackProps> = ({
   children,
   space,
   direction = 'vertical',
+  alignItems = defaultProperty,
+  justifyContent = defaultProperty,
   ...rest
 }) => (
-  <div className={classNames(`Stack Stack--${direction}-${space}`, className)} {...rest}>
+  <div
+    className={classNames(`Stack Stack--${direction}-${space}`, className)}
+    style={{ alignItems, justifyContent }}
+    {...rest}
+  >
     {children}
   </div>
 );
