c6731c6e3d204e93ba6179e949f6e479cc1ff966
Istvan Hevele <istvan.hevele@modaoperandi.com>
2020-05-11 10:17:32 -0400

    feat: credit card number input

---
+++ src/components/CreditCardNumberInput/CreditCardNumberInput.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { CreditCardNumberInput } from './CreditCardNumberInput';
+
+export default { title: 'Components|CreditCardNumberInput' };
+
+export const Default = () => <CreditCardNumberInput label='Card Number' />;
