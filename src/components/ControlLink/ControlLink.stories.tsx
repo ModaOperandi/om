aa7a00583548d203f49c0d3f3e14dc91e821e377
Robert-Mihai Man <robertmihaiman7@gmail.com>
2020-05-22 11:11:53 +0300

    chore(controllink): add controllink stories

--- src/components/ControlLink/ControlLink.stories.tsx
+++ src/components/ControlLink/ControlLink.stories.tsx
@@ -2,23 +2,30 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { States } from 'storybook-states';
 import { ControlLink, ControlLinkProps } from './ControlLink';
+import { BrowserRouter } from 'react-router-dom';
 
 export default { title: 'Components|ControlLink' };
 
-const states = [{}, { disabled: true }];
+const states = [
+  {},
+  { disabled: true },
+  { onClick: action('onClick') },
+  { to: '#href' },
+  { href: '#href' }
+];
 
 export const Underlined = () => (
-  <States<ControlLinkProps> states={states}>
-    <ControlLink onClick={action('onClick')} underlined>
-      Underlined Control Link
-    </ControlLink>
-  </States>
+  <BrowserRouter>
+    <States<ControlLinkProps> states={states}>
+      <ControlLink>Underlined Control Link</ControlLink>
+    </States>
+  </BrowserRouter>
 );
 
 export const Blank = () => (
-  <States<ControlLinkProps> states={states}>
-    <ControlLink onClick={action('onClick')} underlined={false}>
-      Blank Control Link
-    </ControlLink>
-  </States>
+  <BrowserRouter>
+    <States<ControlLinkProps> states={states}>
+      <ControlLink underlined={false}>Blank Control Link</ControlLink>
+    </States>
+  </BrowserRouter>
 );
