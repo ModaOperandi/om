ec21d9c59bc089ff9c643334b1167686bd7aa5c3
Robert-Mihai Man <robertmihaiman7@gmail.com>
2020-06-02 10:30:33 +0300

    fix(breadcrumbs): removes PDPbreadcrumbs as it shouldn't be a diff comp

--- src/components/Breadcrumbs/Breadcrumbs.stories.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { States } from 'storybook-states';
 
 import { Breadcrumbs, BreadcrumbsProps } from './Breadcrumbs';
-import { PdpBreadcrumbs } from './PdpBreadcrumbs';
 
 export default { title: 'Components|Breadcrumbs' };
 
@@ -18,24 +17,14 @@ export const Default = () => (
 );
 
 export const Mobile = () => (
-  <>
-    <States<BreadcrumbsProps>>
-      <Breadcrumbs>
-        <a href='#men'>Men</a>
-        <a href='#clothing'>Clothing</a>
-        <a href='#coats-jackets'>Coats &amp; Jackets</a>
-        Zippy Safari Straight-Leg Wool Jumpsuit
-      </Breadcrumbs>
-    </States>
-    <States<BreadcrumbsProps>>
-      <PdpBreadcrumbs>
-        <a href='#men'>Men</a>
-        <a href='#clothing'>Clothing</a>
-        <a href='#coats-jackets'>Coats &amp; Jackets</a>
-        Zippy Safari Straight-Leg Wool Jumpsuit
-      </PdpBreadcrumbs>
-    </States>
-  </>
+  <States<BreadcrumbsProps>>
+    <Breadcrumbs>
+      <a href='#men'>Men</a>
+      <a href='#clothing'>Clothing</a>
+      <a href='#coats-jackets'>Coats &amp; Jackets</a>
+      Zippy Safari Straight-Leg Wool Jumpsuit
+    </Breadcrumbs>
+  </States>
 );
 
 Mobile.story = { parameters: { viewport: { defaultViewport: 'xs' } } };
