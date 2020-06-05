d663082ff72d2c1b7da13ea2577d3de3b6b237e1
Wayne <waynekaskie@gmail.com>
2020-06-05 12:26:16 -0400

    perf(component): add event emitter for Tabs
    Add onTabClicked to Tabs for calling components to react to tab changes.

--- src/components/Tabs/Tabs.stories.tsx
+++ src/components/Tabs/Tabs.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { States } from 'storybook-states';
 import { Text } from '../Text';
 import { Tabs, TabsProps } from './Tabs';
@@ -9,12 +9,26 @@ const TAB_LIST = [
   {
     name: 'tab1',
     title: 'Tab 1',
-    panel: <Text>First panel</Text>
+    panel: (
+      <Text>
+        FIRST PANEL:
+        <br />
+        Tabs accepts two parameters:
+        <br />- tabs: And array of tab items
+        <br />- onTabClicked: Optional callback when a tab is clicked. Returns the name of the tab
+        that was clicked.
+        <br />
+        <br />
+        <code>
+          {"<Tabs tabs={[ { name: 'tab1', title: 'First Tab', panel: panelComponent } ]} />"}
+        </code>
+      </Text>
+    )
   },
   {
     name: 'tab2',
     title: 'Tab 2',
-    panel: <Text>SecondPanel</Text>
+    panel: <Text>Second panel</Text>
   }
 ];
 
@@ -23,3 +37,20 @@ export const Basic = () => (
     <Tabs tabs={TAB_LIST} />
   </States>
 );
+
+export const withCallback = () => {
+  const [tabName, setTabName] = useState('Tabname will be updated on click');
+  const handleTabClick = (tabName: string) => {
+    setTabName(tabName);
+  };
+
+  return (
+    <States<TabsProps>>
+      <>
+        <Text treatment='h5'>Selected Tab:</Text>
+        <Text> {tabName} </Text>
+        <Tabs tabs={TAB_LIST} onTabClicked={handleTabClick} />
+      </>
+    </States>
+  );
+};
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -5,14 +5,19 @@ import './Tabs.scss';
 
 export type TabsProps = React.HTMLAttributes<HTMLDivElement> & {
   tabs: Tab[];
+  onTabClicked?: (tabName: string) => void;
 };
 
-export const Tabs: React.FC<TabsProps> = ({ tabs, className, ...rest }) => {
+export const Tabs: React.FC<TabsProps> = ({ tabs, onTabClicked, className, ...rest }) => {
   const [activePanel, setActivePanel] = useState(tabs[0].name);
+  const handleTabClick = (tabName: string) => {
+    setActivePanel(tabName);
+    if (onTabClicked) onTabClicked(tabName);
+  };
 
   return (
     <div className={classNames('Tabs', className)} {...rest}>
-      <TabList tabs={tabs} onTabSelected={setActivePanel} activePanel={activePanel} />
+      <TabList tabs={tabs} onTabSelected={handleTabClick} activePanel={activePanel} />
       <TabPanels panels={tabs} activePanel={activePanel} />
     </div>
   );
