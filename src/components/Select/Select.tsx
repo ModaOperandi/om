74221bcbac5d0734289246451a7c7411f2656193
Robert-Mihai Man <robertmihaiman7@gmail.com>
2020-06-22 12:57:08 +0300

    refactor(<select />): refactor select & include undefined option

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useReducer, useEffect, useRef } from 'react';
+import React, { useCallback, useMemo, useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import ChevronDownIcon from '@moda/icons/chevron-down-12';
 import ChevronUpIcon from '@moda/icons/chevron-up-12';
@@ -6,6 +6,7 @@ import { Clickable } from '../Clickable';
 import { SelectOptions } from './SelectOptions';
 import { useClickOutside } from './useClickOutside';
 import { useUpdateEffect } from '../../hooks/useUpdateEffect';
+
 import './Select.scss';
 
 export type SelectableOption = { value: string; label: string; disabled?: boolean };
@@ -23,33 +24,8 @@ export type SelectProps = Omit<React.HTMLAttributes<HTMLDivElement>, 'value' | '
 
 enum Mode {
   Resting,
-  Open,
+  Open
 }
-
-type State = {
-  value: string;
-  focused: string | null;
-  mode: Mode;
-};
-
-type Action =
-  | { type: 'OPEN' }
-  | { type: 'CLOSE' }
-  | { type: 'SELECT'; payload: { value: string } }
-  | { type: 'FOCUS'; payload: { value: string } };
-
-const reducer = (state: State, action: Action) => {
-  switch (action.type) {
-    case 'OPEN':
-      return { ...state, mode: Mode.Open };
-    case 'CLOSE':
-      return { ...state, mode: Mode.Resting };
-    case 'SELECT':
-      return { ...state, value: action.payload.value, focused: null, mode: Mode.Resting };
-    case 'FOCUS':
-      return { ...state, focused: action.payload.value };
-  }
-};
 
 export const Select: React.FC<SelectProps> = ({
   idRef = '',
@@ -63,39 +39,40 @@ export const Select: React.FC<SelectProps> = ({
   onChange,
   ...rest
 }) => {
-  const initialValue =
-    value || defaultValue || options.find((option) => !option.disabled)?.value || options[0].value;
-  const [state, dispatch] = useReducer(reducer, {
-    value: initialValue,
-    focused: initialValue,
-    mode: Mode.Resting,
-  });
+  const initialValue = value || defaultValue;
+
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(initialValue);
+  const [focusedOn, setFocusedOn] = useState<string | undefined>(initialValue);
+  const [mode, setMode] = useState(Mode.Resting);
 
   const selectRef = useRef<HTMLDivElement>(null);
 
   const handleSelect = useCallback(
     (option: SelectableOption) => {
-      dispatch({ type: 'SELECT', payload: { value: option.value } });
+      setSelectedValue(option.value);
+      setFocusedOn(undefined);
+      setMode(Mode.Resting);
       onChange && onChange(option.value);
     },
     [onChange]
   );
 
-  const handleFocus = useCallback(
-    (option: SelectableOption) => dispatch({ type: 'FOCUS', payload: { value: option.value } }),
-    []
-  );
+  const handleFocus = useCallback((option: SelectableOption) => setFocusedOn(option.value), []);
 
-  const handleToggle = useCallback(
-    () => dispatch({ type: state.mode === Mode.Resting ? 'OPEN' : 'CLOSE' }),
-    [state.mode]
-  );
+  const handleToggle = useCallback(() => {
+    setMode((currentMode: Mode) => {
+      if (currentMode === Mode.Open) {
+        return Mode.Resting;
+      }
+      return Mode.Open;
+    });
+  }, []);
 
   const handleKeyDown = useCallback((event: KeyboardEvent) => {
     switch (event.key) {
       case 'Escape': {
         event.preventDefault();
-        return dispatch({ type: 'CLOSE' });
+        return setMode(Mode.Resting);
       }
       default:
         break;
@@ -111,22 +88,20 @@ export const Select: React.FC<SelectProps> = ({
     };
   }, [handleKeyDown]);
 
-  useUpdateEffect(() => {
-    value && dispatch({ type: 'SELECT', payload: { value } });
-  }, [value]);
+  useUpdateEffect(() => setSelectedValue(value), [value]);
 
-  const handleClickOutside = useCallback(() => dispatch({ type: 'CLOSE' }), []);
+  const handleClickOutside = useCallback(() => setMode(Mode.Resting), []);
 
   useClickOutside(selectRef, handleClickOutside);
 
-  const selected = useMemo(() => options.find((option) => state.value === option.value)!, [
+  const selected = useMemo(() => options.find(option => selectedValue === option.value)!, [
     options,
-    state.value,
+    selectedValue
   ]);
 
-  const focused = useMemo(() => options.find((option) => state.focused === option.value)!, [
+  const focused = useMemo(() => options.find(option => focusedOn === option.value)!, [
     options,
-    state.focused,
+    focusedOn
   ]);
 
   return (
@@ -136,24 +111,24 @@ export const Select: React.FC<SelectProps> = ({
       ref={selectRef}
       {...rest}
     >
-      {name && <input id={idRef} name={name} type='hidden' value={state.value} />}
+      {name && <input id={idRef} name={name} type='hidden' value={selectedValue} />}
       <Clickable
         id={`Select__value--${idRef}`}
         className='Select__value'
         disabled={disabled}
         onClick={handleToggle}
         aria-haspopup='listbox'
-        aria-expanded={state.mode === Mode.Open}
+        aria-expanded={mode === Mode.Open}
         aria-labelledby={`Select__label--${idRef} Select__value--${idRef}`}
         type='button'
       >
         <label id={`Select__label--${idRef}`}>{label}</label> {(focused ?? selected)?.label}
         <span className='Select__icon'>
-          {state.mode === Mode.Open ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          {mode === Mode.Open ? <ChevronUpIcon /> : <ChevronDownIcon />}
         </span>
       </Clickable>
 
-      {state.mode === Mode.Open && (
+      {mode === Mode.Open && (
         <SelectOptions
           idRef={idRef}
           className='Select__options'
