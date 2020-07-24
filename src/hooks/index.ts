da1da16919af9f273e05eceaede72dfef3ab3ec7
Norbert Torok <ntorok@grubhub.com>
2020-07-24 15:35:59 +0300

    feat: add breakpoint hook
    Added a new hook called useBreakpoint

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './useUpdateEffect';
+export * from './useBreakpoint';
---
+++ src/hooks/useBreakpoint.ts
@@ -0,0 +1,40 @@
+import { useState, useCallback, useEffect } from 'react';
+import {
+  generateMediaQuery,
+  Mode as ModeType,
+  Breakpoint as BreakpointType
+} from '../components/Breakpoint/generateMediaQuery';
+
+type At = { at: string };
+type GreaterThan = { gt: string };
+type LessThan = { lt: string };
+
+export const discriminate = (
+  props: At | GreaterThan | LessThan
+): { mode: ModeType; breakpoint: BreakpointType } => {
+  if ('at' in props) return { mode: 'at' as ModeType, breakpoint: props.at as BreakpointType };
+  if ('lt' in props) return { mode: 'lt' as ModeType, breakpoint: props.lt as BreakpointType };
+  return { mode: 'gt' as ModeType, breakpoint: props.gt as BreakpointType };
+};
+
+export const useBreakpoint = (props: At | GreaterThan | LessThan) => {
+  const { mode, breakpoint } = discriminate(props);
+  const mediaQuery = generateMediaQuery({ mode, breakpoint });
+  const [matches, setMatches] = useState(true);
+
+  const handleChange = useCallback(
+    ({ matches }: MediaQueryListEvent): void => setMatches(matches),
+    []
+  );
+
+  useEffect(() => {
+    const mql = window.matchMedia(mediaQuery);
+    mql.addListener(handleChange);
+    setMatches(mql.matches);
+    return (): void => mql.removeListener(handleChange);
+  }, [handleChange, mediaQuery]);
+
+  return {
+    matches
+  };
+};
