5d2ef434fa4c6c288ca402c6514cf77360f3c674
Robert-Mihai Man <robertmihaiman7@gmail.com>
2020-08-10 16:55:52 +0300

    fix(modal overlay): fix modal overlay

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,48 +1,21 @@
-import React, { useEffect, useRef } from 'react';
-import { createPortal } from 'react-dom';
+import React, { useRef } from 'react';
 import { FocusOn } from 'react-focus-on';
 import classNames from 'classnames';
-import { Overlay, OverlayProps } from '../Overlay';
 
 import './Modal.scss';
 
 export type ModalProps = React.HTMLAttributes<HTMLDivElement> & {
   onClose(): void;
-  overlay?: boolean;
 };
 
-export const Modal: React.FC<ModalProps & OverlayProps> = ({
-  className,
-  children,
-  onClose,
-  overlay,
-  ...rest
-}) => {
+export const Modal: React.FC<ModalProps> = ({ className, children, onClose, ...rest }) => {
   const wrapper = useRef(null);
-  const el = useRef(document.createElement('div'));
 
-  useEffect(() => {
-    const _el = el.current;
-
-    document.body.appendChild(_el);
-
-    return () => {
-      _el && _el.parentElement && _el.parentElement.removeChild(_el);
-    };
-  }, []);
-
-  const Component = overlay ? Overlay : 'div';
-
-  return createPortal(
-    <Component
-      className={classNames('Modal', { 'Modal--overlay': overlay }, className)}
-      ref={wrapper}
-      {...rest}
-    >
+  return (
+    <div className={classNames('Modal', className)} ref={wrapper} {...rest}>
       <FocusOn onClickOutside={onClose} onEscapeKey={onClose}>
         {children}
       </FocusOn>
-    </Component>,
-    el.current
+    </div>
   );
 };
--- src/components/ModalOverlay/ModalOverlay.tsx
+++ src/components/ModalOverlay/ModalOverlay.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import classNames from 'classnames';
 import { Modal, ModalProps } from '../Modal';
-import { OverlayProps } from '../Overlay';
+import { OverlayProps, Overlay } from '../Overlay';
+import classNames from 'classnames';
 
 import './ModalOverlay.scss';
 
 export type ModalOverlayProps = ModalProps & OverlayProps;
 
-export const ModalOverlay: React.FC<ModalOverlayProps> = ({ children, className, ...rest }) => (
-  <Modal overlay {...rest}>
-    <div className={classNames('ModalOverlay', className)}>{children}</div>
-  </Modal>
+export const ModalOverlay: React.FC<ModalOverlayProps> = ({
+  children,
+  className,
+  onClose,
+  show,
+  ...rest
+}) => (
+  <Overlay className={classNames('ModalOverlay', className)} show={show} {...rest}>
+    <Modal className='ModalOverlay__modal' onClose={onClose}>
+      <div className='ModalOverlay__content'>{children}</div>
+    </Modal>
+  </Overlay>
 );
