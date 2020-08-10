349c5bab3ddac8a2cd340806e81f9eafd65c187f
Robert-Mihai Man <robertmihaiman7@gmail.com>
2020-08-10 19:29:29 +0300

    fix(modal overlay): fix modal overlay

--- src/components/ModalOverlay/ModalOverlay.tsx
+++ src/components/ModalOverlay/ModalOverlay.tsx
@@ -6,12 +6,15 @@ import classNames from 'classnames';
 import './ModalOverlay.scss';
 import { createPortal } from 'react-dom';
 
-export type ModalOverlayProps = ModalProps & OverlayProps;
+export type ModalOverlayProps = ModalProps &
+  OverlayProps & { overlayClassName?: string; contentClassName?: string };
 
 export const ModalOverlay: React.FC<ModalOverlayProps> = ({
   children,
   className,
+  contentClassName,
   onClose,
+  overlayClassName,
   show,
   ...rest
 }) => {
@@ -26,9 +29,9 @@ export const ModalOverlay: React.FC<ModalOverlayProps> = ({
   }, []);
 
   return createPortal(
-    <Overlay className={classNames('ModalOverlay', className)} show={show} {...rest}>
-      <Modal className='ModalOverlay__modal' onClose={onClose}>
-        <div className='ModalOverlay__content'>{children}</div>
+    <Overlay className={classNames('ModalOverlay', overlayClassName)} show={show} {...rest}>
+      <Modal className={classNames('ModalOverlay__modal', className)} onClose={onClose}>
+        <div className={classNames('ModalOverlay__content', contentClassName)}>{children}</div>
       </Modal>
     </Overlay>,
     el.current
