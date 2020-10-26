60f7b9b3cd1e7758b869bcbff4f4c04f1bc89dd1
Robert-Mihai Man <robertmihaiman7@gmail.com>
2020-10-26 13:18:38 +0200

    fix(radio-button): fix roles

--- src/components/RadioButton/RadioButton.tsx
+++ src/components/RadioButton/RadioButton.tsx
@@ -13,36 +13,34 @@ export const RadioButton: React.FC<RadioButtonProps> = ({
   value,
   tabIndex,
   ...rest
-}) => {
-  return (
-    <label
-      role='radio'
+}) => (
+  <label
+    tabIndex={-1}
+    className={classNames('RadioButton', className, {
+      'RadioButton--checked': checked
+    })}
+  >
+    <span
+      tabIndex={-1}
+      className={`RadioButton__indicator RadioButton__indicator--${
+        checked ? 'checked' : 'unchecked'
+      }`}
+    />
+
+    <input
       tabIndex={tabIndex != null ? tabIndex : 0}
-      className={classNames('RadioButton', className, {
-        'RadioButton--checked': checked
-      })}
-    >
-      <span
-        tabIndex={-1}
-        className={`RadioButton__indicator RadioButton__indicator--${
-          checked ? 'checked' : 'unchecked'
-        }`}
-      />
+      className='RadioButton__input'
+      type='radio'
+      role='radio'
+      checked={checked}
+      value={value}
+      {...rest}
+    />
 
-      <input
-        tabIndex={-1}
-        className='RadioButton__input'
-        type='radio'
-        checked={checked}
-        value={value}
-        {...rest}
-      />
-
-      {children && (
-        <span className='RadioButton__label' tabIndex={-1}>
-          {children}
-        </span>
-      )}
-    </label>
-  );
-};
+    {children && (
+      <span className='RadioButton__label' tabIndex={-1}>
+        {children}
+      </span>
+    )}
+  </label>
+);
