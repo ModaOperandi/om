1c19687895aed709431d4e73c8f0f36ecc37c022
Robert-Mihai Man <robertmihaiman7@gmail.com>
2020-10-28 18:16:14 +0200

    feat(badge): add fall-25 theme

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,35 +1,17 @@
 import React from 'react';
 import classNames from 'classnames';
+import { Clickable } from '../Clickable';
 
 import './Badge.scss';
 
-export const TRENDING_THEME = 'trending';
-export const BESTSELLER_THEME = 'bestseller';
-export const FALL_ESSENTIAL_THEME = 'fall-essential';
+type Theme = 'bestseller' | 'trending' | 'fall-essential' | 'fall-25';
 
-const THEMES = {
-  [BESTSELLER_THEME]: 'Bestseller',
-  [TRENDING_THEME]: 'Trending',
-  [FALL_ESSENTIAL_THEME]: 'Fall Essential'
-} as const;
-
-type Theme = keyof typeof THEMES;
-type CustomProps = { children: React.ReactNode; theme?: string }; //if a theme is passed in that is not an expected theme, treat this as a custom badge
-type ThemeProps = { theme: Theme };
-
-const isThemed = (props: CustomProps | ThemeProps): props is ThemeProps =>
-  'theme' in props && props.theme !== undefined && Object.keys(THEMES).indexOf(props.theme) > -1;
-
-export type BadgeProps = (CustomProps | ThemeProps) & React.AnchorHTMLAttributes<HTMLAnchorElement>;
-
-export const Badge: React.FC<BadgeProps> = ({ className, ...rest }) => {
-  if (isThemed(rest)) {
-    const { theme, ...remaining } = rest;
-    return (
-      <a className={classNames(`Badge Badge--${theme}`, className)} {...remaining}>
-        {THEMES[theme]}
-      </a>
-    );
-  }
-  return <a className={classNames('Badge', className)} {...rest} />;
+export type BadgeProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+  theme?: Theme;
 };
+
+export const Badge: React.FC<BadgeProps> = ({ className, theme, children, ...rest }) => (
+  <Clickable className={classNames('Badge', theme && `Badge--${theme}`, className)} {...rest}>
+    {children}
+  </Clickable>
+);
