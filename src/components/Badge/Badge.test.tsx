4eb8ccd0e373eee0dec6bcc26ecc1d4640112f51
Robert-Mihai Man <robertmihaiman7@gmail.com>
2020-10-29 09:58:05 +0200

    fix(badge): make badge a span and not a link (#1040)

--- src/components/Badge/Badge.test.tsx
+++ src/components/Badge/Badge.test.tsx
@@ -4,9 +4,14 @@ import { shallow } from 'enzyme';
 import { Badge } from './Badge';
 
 describe('Badge', () => {
-  it('supports preeet themes', () => {
-    const component = shallow(<Badge theme='fall-essential'>Fall Essential</Badge>);
-    expect(component.text()).toEqual('Fall Essential');
+  it('supports custom children', () => {
+    const component = shallow(<Badge>Custom Text - 1</Badge>);
+    expect(component.html()).toEqual('<span class="Badge">Custom Text - 1</span>');
+  });
+
+  it('supports custom themes and children', () => {
+    const component = shallow(<Badge theme='trending'>Custom Text - 2</Badge>);
+    expect(component.html()).toEqual('<span class="Badge Badge--trending">Custom Text - 2</span>');
   });
 
   it('allows for arbitrary children', () => {
