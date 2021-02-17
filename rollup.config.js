192515d4cb282b756d3d407e9b8b47339c5a3782
Istvan Hevele <istvan.hevele@modaoperandi.com>
2021-02-17 09:12:59 -0500

    fix: css order

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
-import scss from 'rollup-plugin-scss';
-import autoprefixer from 'autoprefixer';
-import postcss from 'postcss';
-import nodeSass from 'node-sass';
+import postcss from '@moda/rollup-plugin-postcss';
 import packageImporter from 'node-sass-package-importer';
 import pkg from './package.json';
 
@@ -29,17 +26,17 @@ export default {
     resolve({ extensions: ['.js', '.jsx', '.ts', '.tsx'] }),
     commonjs(),
     babel({ babelHelpers: 'bundled', extensions: ['.js', '.jsx', '.ts', '.tsx'] }),
-    scss({
-      output: 'dist/styles.css',
-      processor: css =>
-        postcss([autoprefixer])
-          .process(css)
-          .then(result => result.css),
-      sass: nodeSass,
-      importer: [
-        packageImporter(),
-        url => (url === '~om' ? { file: path.join(__dirname, 'src/index.scss') } : null)
-      ]
+    postcss({
+      extract: 'styles.css',
+      minimize: true,
+      use: {
+        sass: {
+          importer: [
+            packageImporter(),
+            url => (url === '~om' ? { file: path.join(__dirname, 'src/index.scss') } : null)
+          ]
+        }
+      }
     })
   ]
 };
