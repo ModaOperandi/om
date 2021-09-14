eaea22f5c8ce4af79c8450bc66f20e375a5e94b7
natalia.alesencova <natalia.alesencova@modaoperandi.com>
2021-09-14 10:59:55 -0400

    feat(input): new password input component

---
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Clickable } from '../Clickable';
+import { Input, FieldProps } from '../Field';
+import { Text } from '../Text';
+
+import './PasswordInput.scss';
+
+export type PasswordInputProps = {
+  dataTestId?: string;
+} & FieldProps;
+
+export const PasswordInput: React.FC<PasswordInputProps> = ({
+  error,
+  focus,
+  dataTestId,
+  id,
+  placeholder
+}) => {
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
+
+  return (
+    <div className='PasswordInput'>
+      <Input
+        name='password'
+        placeholder={placeholder}
+        type={passwordShown ? 'text' : 'password'}
+        error={error}
+        focus={focus}
+        id={id}
+        data-testid={dataTestId}
+      />
+      <Clickable
+        className='PasswordInput__password-action'
+        onClick={togglePasswordVisiblity}
+        type='button'
+      >
+        {passwordShown ? (
+          <Text treatment='eyebrow'>HIDE</Text>
+        ) : (
+          <Text treatment='eyebrow'>SHOW</Text>
+        )}
+      </Clickable>
+    </div>
+  );
+};
