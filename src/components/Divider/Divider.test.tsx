3ce91ca5a201c779ca6cc061fd7be0939c4f2fe0
Istvan Hevele <istvan.hevele@modaoperandi.com>
2021-09-30 13:08:09 +0300

    feat(divider): allow specifying text treatment (#2028)

--- src/components/Divider/Divider.test.tsx
+++ src/components/Divider/Divider.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import { Divider } from './Divider';
 
 describe('Divider', () => {
   it('renders correctly', () => {
-    const component = shallow(<Divider style={{ padding: '30px 0' }} text='Hello' />);
+    const component = mount(<Divider style={{ padding: '30px 0' }} text='Hello' />);
     expect(component.text()).toEqual('Hello');
-    expect(component.html()).toContain('style="padding:30px 0"');
+    expect(component.html()).toContain('style="padding: 30px 0px;"');
   });
 });
