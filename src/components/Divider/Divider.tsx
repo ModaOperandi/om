53e13d8edeadcc6e00d3f8ea0d9a6acc7577c4f9
Istvan Hevele <istvan.hevele@modaoperandi.com>
2021-09-30 05:56:25 -0400

    feat(divider): allow specifying text treatment

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import classNames from 'classnames';
+import { Text, TextTreatment, TextColor, TextFontFamily } from '../Text';
 
 import './Divider.scss';
 
 export type DividerProps = React.HTMLAttributes<HTMLDivElement> & {
   text?: string;
+  treatment?: TextTreatment;
+  color?: TextColor;
+  family?: TextFontFamily;
   type?: 'no-line' | 'two-line';
   verticalPadding?: string;
 };
 
-export const Divider: React.FC<DividerProps> = ({ text, type, className, ...rest }) => (
-  <div className={classNames('Divider', type ? `Divider--${type}` : '', className)} {...rest}>
-    {text && <div className='Divider__text'>{text}</div>}
-  </div>
-);
+export const Divider: React.FC<DividerProps> = ({
+  text,
+  treatment,
+  color,
+  family,
+  type,
+  className,
+  ...rest
+}) => {
+  const defaultTreatment = type === 'two-line' ? 'eyebrow' : 'h5';
+  const defaultFamily = type === 'two-line' ? undefined : 'serif';
+
+  return (
+    <div className={classNames('Divider', type ? `Divider--${type}` : '', className)} {...rest}>
+      {text && (
+        <Text
+          className='Divider__text'
+          treatment={treatment ?? defaultTreatment}
+          color={color}
+          family={family ?? defaultFamily}
+        >
+          {text}
+        </Text>
+      )}
+    </div>
+  );
+};
