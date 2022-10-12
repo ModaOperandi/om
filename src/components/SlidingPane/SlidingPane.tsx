ffb5ab77a5dcb105693efb9a338b56c41bba86a4
Istvan Hevele <istvan.hevele@modaoperandi.com>
2022-10-12 15:56:07 +0300

    feat(SlidingPane): lock focus (#2965)

--- src/components/SlidingPane/SlidingPane.tsx
+++ src/components/SlidingPane/SlidingPane.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import classNames from 'classnames';
 import ExitIcon from '@moda/icons/exit-16';
+import { FocusOn } from 'react-focus-on';
 import { Clickable } from '../Clickable';
 import { Text } from '../Text';
-
-import './SlidingPane.scss';
 import { Stack } from '../Stack';
+import './SlidingPane.scss';
 
 export type SlidingPaneProps = React.HTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode;
@@ -25,33 +25,35 @@ export const SlidingPane: React.FC<SlidingPaneProps> = ({
   return (
     <>
       <div
-        className={classNames('SlidingPane', className, {
-          'SlidingPane--active': visible
-        })}
-        {...rest}
-      >
-        <Stack
-          space={0}
-          justifyContent='center'
-          direction='horizontal'
-          className='SlidingPane__top'
-        >
-          <Text treatment='h5' family='serif'>
-            {title}
-          </Text>
-          <Clickable className='SlidingPane__close' onClick={onClose}>
-            <ExitIcon />
-          </Clickable>
-        </Stack>
-        <div className='SlidingPane__content'>{children}</div>
-      </div>
-
-      <div
         className={classNames('SlidingPane__overlay', {
           'SlidingPane__overlay--active': visible
         })}
         onClick={onClose}
       ></div>
+
+      <FocusOn enabled={visible} onClickOutside={onClose} onEscapeKey={onClose}>
+        <div
+          className={classNames('SlidingPane', className, {
+            'SlidingPane--active': visible
+          })}
+          {...rest}
+        >
+          <Stack
+            space={0}
+            justifyContent='center'
+            direction='horizontal'
+            className='SlidingPane__top'
+          >
+            <Text treatment='h5' family='serif'>
+              {title}
+            </Text>
+            <Clickable className='SlidingPane__close' onClick={onClose}>
+              <ExitIcon />
+            </Clickable>
+          </Stack>
+          <div className='SlidingPane__content'>{children}</div>
+        </div>
+      </FocusOn>
     </>
   );
 };
