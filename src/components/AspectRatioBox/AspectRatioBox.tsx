58c5beed1db5aef6f56833c7eac84153052c20d8
Istvan Hevele <istvan.hevele@modaoperandi.com>
2023-01-09 11:31:09 -0500

    feat(AspectRatioBox): make maxWidth and maxHeight optional

--- src/components/AspectRatioBox/AspectRatioBox.tsx
+++ src/components/AspectRatioBox/AspectRatioBox.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import { scale } from 'proportional-scale';
+import { scale, paddingBottom } from 'proportional-scale';
 import './AspectRatioBox.scss';
 
 export const DEFAULT_PRODUCT_ASPECT_WIDTH = 128;
@@ -11,9 +11,29 @@ export const DEFAULT_PRODUCT_ASPECT_RATIO =
 export type AspectRatioBoxProps = React.HTMLAttributes<HTMLDivElement> & {
   aspectWidth?: number;
   aspectHeight?: number;
-  maxWidth: number;
-  maxHeight: number;
+  maxWidth?: number;
+  maxHeight?: number;
   outlined?: boolean;
+};
+
+const scaleDimensions = ({
+  width,
+  height,
+  maxWidth,
+  maxHeight
+}: {
+  width: number;
+  height: number;
+  maxWidth?: number;
+  maxHeight?: number;
+}) => {
+  if (maxWidth && maxHeight) return scale({ width, height, maxWidth, maxHeight });
+
+  if (maxWidth) return scale({ width, height, maxWidth });
+
+  if (maxHeight) return scale({ width, height, maxHeight });
+
+  return { width, height, paddingBottom: paddingBottom({ width, height }), scale: 1 };
 };
 
 export const AspectRatioBox = React.forwardRef(
@@ -30,7 +50,7 @@ export const AspectRatioBox = React.forwardRef(
     }: AspectRatioBoxProps,
     forwardedRef: React.Ref<HTMLDivElement>
   ) => {
-    const { width, paddingBottom } = scale({
+    const { width, paddingBottom } = scaleDimensions({
       width: aspectWidth,
       height: aspectHeight,
       maxWidth,
@@ -47,7 +67,7 @@ export const AspectRatioBox = React.forwardRef(
           },
           className
         )}
-        style={{ maxWidth: `${width}px` }}
+        style={maxWidth || maxHeight ? { maxWidth: `${width}px` } : undefined}
         {...rest}
       >
         <div className='AspectRatioBox__wrapper' style={{ paddingBottom }} />
